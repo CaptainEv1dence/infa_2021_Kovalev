7120df55d87111cfa6cd8a434f2063cf8bffd268
Kovalev Petr <kovalev.pa@phystech.edu>
2021-10-11 17:12:33 +0300

    modelka kolca

--- lab6/ex1.py
+++ lab6/ex1.py
@@ -10,7 +10,10 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((600, 600))
 COLORS = [(0, 0 , 0), (0, 255 , 255), (255, 0 , 255), (255, 255 , 0), (0, 0 , 255), (0, 255 , 0), (255, 0 , 0)]
+WHITE = (255, 255, 255)
 SCORE = 0
+rings = 3
+balls = 12
 
 class Ball:
 
@@ -44,15 +47,53 @@ class Ball:
             return 0
 
 
+class Ring:
+
+    def __init__(self):
+
+        self.v_x = randint(-100, 100)/10
+        self.v_y = randint(-100, 100)/10
+        self.r2 = randint(30, 40)
+        self.r1 = 2 * self.r2 / 3
+        self.x = randint(self.r2, 580 - self.r2)
+        self.y = randint(self.r2, 580 - self.r2)
+        self.color = (255, 0, 0)
+
+    def draw(self, surface):
+        circle(surface, self.color, (self.x, self.y), self.r2)
+        circle(surface, WHITE, (self.x, self.y), self.r1)
+
+    def move(self):
+        self.x += self.v_x + randint(-100, 100)/40
+        self.y += self.v_y + randint(-100, 100)/40
+
+    def collision(self, borders):
+        if not (self.x > self.r2 and self.x < borders[0]):
+            self.v_x = - self.v_x
+
+        if not (self.y > self.r2 and self.y < borders[1]):
+            self.v_y = - self.v_y
+
+    def shot(self, x, y):
+        if (x - self.x) ** 2 + (y - self.y) ** 2 <= self.r2 ** 2 and (x - self.x) ** 2 + \
+                (y - self.y) ** 2 >= self.r1 ** 2:
+            return 100
+        else:
+            return 0
+
+
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
 
-pool = [Ball()]
-for i in range(14):
-    pool.append(Ball())
+ball_pool = []
+ring_pool = []
 
+for i in range(balls):
+    ball_pool.append(Ball())
+for i in range(rings):
+    ring_pool.append(Ring())
 
 while not finished:
     clock.tick(FPS)
@@ -62,17 +103,30 @@ while not finished:
             finished = True
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
+
             print('BANG!')
-            for ball in pool:
+
+            for ball in ball_pool:
                 if (ball.shot(event.pos[0], event.pos[1]) == 100):
                     SCORE += 100
                     print(f"SCORE = {SCORE}")
                     print('NICE SHOT!')
 
-    for ball in pool:
+            for ring in ring_pool:
+                if (ring.shot(event.pos[0], event.pos[1]) == 100):
+                    SCORE += 500
+                    print(f"SCORE = {SCORE}")
+                    print('AMAZING!')
+
+    for ball in ball_pool:
         ball.draw(screen)
         ball.move()
         ball.collision([580, 580])
+
+    for ring in ring_pool:
+        ring.draw(screen)
+        ring.move()
+        ring.collision([580, 580])
 
     pygame.display.update()
     screen.fill((255, 255, 255))
